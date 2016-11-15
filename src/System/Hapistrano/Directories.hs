fb24fef4289407438845d6c89364b2a2ad327fbb
Sebastián Estrella <sebas007estrella@gmail.com>
2016-11-15 14:09:08 -0500

    Initial project bootstrap

--- src/System/Hapistrano/Directories.hs
+++ src/System/Hapistrano/Directories.hs
@@ -1,18 +1,49 @@
+{-# LANGUAGE RecordWildCards #-}
+
 module System.Hapistrano.Directories where
 
 import           Control.Monad.Reader
---import           Data.Text            (Text)
---import qualified Data.Text as T (unpack)
 import           Development.Shake
 
-{-
-main' :: IO ()
-main' = shake shakeOptions $ do
-  setupDirs $ RemoteHost "welly" 
--}
+data Config = Config
+  { deployPath :: FilePath
+  , repository :: String
+  } deriving (Eq, Show)
 
+main :: IO ()
+main = shake shakeOptions $ do
+  let config =
+        Config
+          { repository = "git@github.com:stackbuilders/hapistrano.git"
+          , deployPath = "tmp"
+          }
+  action $ do
+    runReaderT createOrUpdateRepo config
 
-data Config = undefined
+createOrUpdateRepo :: ReaderT Config Action ()
+createOrUpdateRepo = do
+  Config{..} <- ask
+  let repoPath = deployPath ++ "/repo"
+  exists <- lift $ doesDirectoryExist repoPath
+  lift $ if exists
+    then updateRepo repoPath
+    else createRepo repository repoPath
 
-ensureRepositoryPushed :: Config -> Either String String 
-ensureRepositoryPushed = undefined 
+createRepo :: String -> FilePath -> Action ()
+createRepo repository repoPath =
+  cmd "git clone --bare" [repository, repoPath]
+
+updateRepo :: String -> Action ()
+updateRepo repoPath =
+  cmd [Cwd repoPath] "git fetch origin +refs/heads/*:refs/heads/*"
+
+createRelease :: ReaderT Config Action String
+createRelease = do
+  deployPath <- asks deployPath
+  release <- liftIO getTimestamp
+  let releasePath = deployPath ++ "/releases/" ++ release
+  lift (cmd "git clone " [deployPath ++ "/repo", releasePath] :: Action ())
+  return releasePath
+
+getTimestamp :: IO String
+getTimestamp = return "2016115"
