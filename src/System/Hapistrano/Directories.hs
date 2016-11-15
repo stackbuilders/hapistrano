4db3e92e58e6bc66c55243f0126e792126344c7a
sebastian <sebastian0092@gmail.com>
2016-11-15 17:36:32 -0500

    chaging timestamps, implementing setReleaseRevision and implementing readCurrentLink

--- src/System/Hapistrano/Directories.hs
+++ src/System/Hapistrano/Directories.hs
@@ -4,22 +4,35 @@ module System.Hapistrano.Directories where
 
 import           Control.Monad.Reader
 import           Development.Shake
+import           Data.List (intercalate)
+import qualified Data.Time as TM
+
+
 
 data Config = Config
   { deployPath :: FilePath
   , repository :: String
+  , revision :: String
   } deriving (Eq, Show)
 
-main :: IO ()
-main = shake shakeOptions $ do
+type Release = String
+type ReleasePath = String 
+
+
+main' :: IO ()
+main' = shake shakeOptions $ do
   let config =
         Config
           { repository = "git@github.com:stackbuilders/hapistrano.git"
           , deployPath = "tmp"
+          , revision = " origin/staging"
           }
-  action $ do
-    runReaderT createOrUpdateRepo config
+  action $ (`runReaderT` config) $ do
+    createOrUpdateRepo
+    stamp <- createRelease
+    setReleaseRevison stamp 
 
+    
 createOrUpdateRepo :: ReaderT Config Action ()
 createOrUpdateRepo = do
   Config{..} <- ask
@@ -37,13 +50,43 @@ updateRepo :: String -> Action ()
 updateRepo repoPath =
   cmd [Cwd repoPath] "git fetch origin +refs/heads/*:refs/heads/*"
 
-createRelease :: ReaderT Config Action String
+createRelease :: ReaderT Config Action ReleasePath
 createRelease = do
   deployPath <- asks deployPath
   release <- liftIO getTimestamp
   let releasePath = deployPath ++ "/releases/" ++ release
   lift (cmd "git clone " [deployPath ++ "/repo", releasePath] :: Action ())
-  return releasePath
+  return release --releasePath
 
-getTimestamp :: IO String
-getTimestamp = return "2016115"
+getTimestamp :: IO Release
+getTimestamp = do
+  (TM.UTCTime _ time) <- TM.getCurrentTime
+  return $ show (time * 1000000)
+
+currentPath :: FilePath -> FilePath
+currentPath depPath = depPath ++ "/releases" 
+
+releasesPath :: Config -> FilePath
+releasesPath conf = deployPath conf ++ "/releases"  
+
+releasePath :: Config -> Release -> FilePath
+releasePath conf rel = releasesPath conf ++ "/" ++ rel 
+
+
+setReleaseRevison :: Release -> ReaderT Config Action Release
+setReleaseRevison rel = do
+  conf <- ask
+  liftIO $ putStrLn "Setting revision in release path."
+  lift $ (cmd $ intercalate " && " [ "cd " ++ "releases/" ++ rel
+                         , "git fetch --all"
+                         , "git reset --hard" ++ revision conf
+                         ] :: Action ())
+  return rel  
+
+  
+readCurrentLink :: ReaderT Config Action String
+readCurrentLink = do
+  conf <- ask
+  let releasePath = (currentPath . deployPath) conf 
+  lift (cmd $ "readlink " ++ "tmp/current" :: Action ()) 
+  return releasePath
