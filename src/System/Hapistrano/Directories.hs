e39a179c68ba4d804c935dad85bdfc7e3fdc9119
sebastian <sebastian0092@gmail.com>
2016-11-15 09:52:53 -0500

    cleaning experimental shake-extension and adding helpers

--- src/System/Hapistrano/Directories.hs
+++ src/System/Hapistrano/Directories.hs
@@ -1,26 +1,13 @@
-{-# LANGUAGE OverloadedStrings #-}
-
 module System.Hapistrano.Directories where
 
 import           Control.Monad.Reader
-import           Data.Text            (Text)
+--import           Data.Text            (Text)
+--import qualified Data.Text as T (unpack)
 import           Development.Shake
 
-newtype RemoteHost = RemoteHost { unRemoteHost :: Text }
-  deriving (Eq, Show)
+{-
+main' :: IO ()
+main' = shake shakeOptions $ do
+  setupDirs $ RemoteHost "welly" 
+-}
 
-main :: IO ()
-main = shake shakeOptions $ do
-  setupDirs
-
-
-setupDirs :: Rules ()
-setupDirs =
-  action $ do
-    unit $ cmd "ssh server mkdir -p repo"
-    unit $ cmd "ssh server mkdir -p releases"
-
-myCmd :: Text -> [Text] -> ReaderT RemoteHost Action a
-myCmd _ _ = do
-  hostName <- asks unRemoteHost
-  undefined
---
+++ src/System/Hapistrano/Helpers.hs
@@ -0,0 +1,2 @@
+module System.Hapistrano.Helpers where
+
