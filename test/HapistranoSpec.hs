10b7efd8a65c433c548334a7d597d792c638c35e
Sebastián Estrella <sebas007estrella@gmail.com>
2016-11-15 22:43:32 -0500

    Add test for cache repo

--- test/HapistranoSpec.hs
+++ test/HapistranoSpec.hs
@@ -11,10 +11,16 @@ import           Hapistrano
 
 spec :: Spec
 spec =
-  describe "deploy" $
-    it "symlinks the latest release on the current directory" $ withDeployTo $ \deployTo -> do
-      deploy $ config deployTo
-      isSymbolicLink (unCurrentPath $ getCurrentPath deployTo) `shouldReturn` True
+  describe "deploy" $ do
+    it "creates a cache repo" $
+      withDeployTo $ \deployTo -> do
+        deploy $ config deployTo
+        doesDirectoryExist (unRepoPath $ getRepoPath deployTo) `shouldReturn` True
+
+    it "symlinks the latest release on the current directory" $
+      withDeployTo $ \deployTo -> do
+        deploy $ config deployTo
+        isSymbolicLink (unCurrentPath $ getCurrentPath deployTo) `shouldReturn` True
 
 withDeployTo :: (DeployTo -> IO a) -> IO a
 withDeployTo f = withSystemTempDirectory "hapistrano" (f . DeployTo)
