f8a869ce3db647914333e05a441abda401693dbc
William R. Arellano <arellanowr@gmail.com>
2019-11-11 13:01:17 -0500

    Quickcheck property tests (#128)
    * Add property check quote command
    
    * Add property test trim function
    
    * Fix biased test cases for trim function
    
    * Stack lock
    
    * Separated property tests from other tests
    
    * Fixes on imports
    
    * PR quickcheck
    
    * Remove wrong comment
    
    * Add property test for generic and ungeneric command
    
    * Refactoring isCmdString function
    
    * Fix conflicts
    
    * Add quickchek part 1

---
+++ spec/System/HapistranoPropsSpec.hs
@@ -0,0 +1,85 @@
+module System.HapistranoPropsSpec
+  ( spec
+  ) where
+
+import           Data.Char                           (isSpace)
+import           System.Hapistrano.Commands.Internal (mkGenericCommand,
+                                                      quoteCmd, trim,
+                                                      unGenericCommand)
+import           Test.Hspec                          hiding (shouldBe,
+                                                      shouldReturn)
+import           Test.QuickCheck
+
+spec :: Spec
+spec =
+  describe "QuickCheck" $
+  context "Properties" $ do
+    it "property of quote command" $ property propQuote'
+    it "property of trimming a command" $
+      property $ forAll trimGenerator propTrim'
+    it "property of mkGenericCommand and unGenericCommand" $
+      property $ forAll genericCmdGenerator propGenericCmd'
+
+-- Is quoted determine
+isQuoted :: String -> Bool
+isQuoted str = head str == '"' && last str == '"'
+
+-- | Quote function property
+propQuote :: String -> Bool
+propQuote str =
+  if any isSpace str
+    then isQuoted $ quoteCmd str
+    else quoteCmd str == str
+
+propQuote' :: String -> Property
+propQuote' str =
+  classify (any isSpace str) "has at least a space" $ propQuote str
+
+-- | Is trimmed
+isTrimmed' :: String -> Bool
+isTrimmed' [] = True
+isTrimmed' [x] = not $ isSpace x
+isTrimmed' str =
+  let a = not . isSpace $ head str
+      b = not . isSpace $ last str
+   in a && b
+
+-- | Prop trimm
+propTrim :: String -> Bool
+propTrim = isTrimmed' . trim
+
+propTrim' :: String -> Property
+propTrim' str =
+  classify (not $ isTrimmed' str) "non trimmed strings" $ propTrim str
+
+-- | Check that the string is perfect command String
+isCmdString :: String -> Bool
+isCmdString str = all ($str) [not . null, notElem '#', notElem '\n', isTrimmed']
+
+-- | Prop Generic Command
+-- If the string does not contain # or \n, is trimmed and non null, the command should be created
+propGenericCmd :: String -> Bool
+propGenericCmd str =
+  if isCmdString str
+    then maybe False ((== str) . unGenericCommand) (mkGenericCommand str)
+    else maybe True ((/= str) . unGenericCommand) (mkGenericCommand str) -- Either the command cannot be created or the command str is different to the original
+
+propGenericCmd' :: String -> Property
+propGenericCmd' str =
+  classify (isCmdString str) "perfect command string" propGenericCmd
+
+-- | Trim String Generator
+trimGenerator :: Gen String
+trimGenerator =
+  let strGen = listOf arbitraryUnicodeChar
+   in frequency
+        [ (1, suchThat strGen isTrimmed')
+        , (1, suchThat strGen (not . isTrimmed'))
+        ]
+
+-- | Generic Command generator
+genericCmdGenerator :: Gen String
+genericCmdGenerator =
+  let strGen = listOf $ elements $ ['A'..'Z'] ++ ['a'..'z'] ++ [' ', '#', '*', '/', '.']
+   in frequency
+        [(1, suchThat strGen isCmdString), (1, suchThat strGen (elem '#'))]
--- src/System/Hapistrano/Core.hs
+++ src/System/Hapistrano/Core.hs
@@ -9,7 +9,6 @@
 --
 -- Core Hapistrano functions that provide basis on which all the
 -- functionality is built.
-
 {-# LANGUAGE OverloadedStrings   #-}
 {-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
@@ -39,28 +38,30 @@ import           System.Process.Typed       (ProcessConfig)
 import qualified System.Process.Typed       as SPT
 
 -- | Run the 'Hapistrano' monad. The monad hosts 'exec' actions.
-
-runHapistrano :: MonadIO m
-  => Maybe SshOptions  -- ^ SSH options to use or 'Nothing' if we run locally
-  -> Shell             -- ^ Shell to run commands
+runHapistrano ::
+     MonadIO m
+  => Maybe SshOptions -- ^ SSH options to use or 'Nothing' if we run locally
+  -> Shell -- ^ Shell to run commands
   -> (OutputDest -> String -> IO ()) -- ^ How to print messages
-  -> Hapistrano a      -- ^ The computation to run
-  -> m (Either Int a)  -- ^ Status code in 'Left' on failure, result in
+  -> Hapistrano a -- ^ The computation to run
+  -> m (Either Int a) -- ^ Status code in 'Left' on failure, result in
               -- 'Right' on success
-runHapistrano sshOptions shell' printFnc m = liftIO $ do
-  let config = Config
-        { configSshOptions = sshOptions
-        , configShellOptions = shell'
-        , configPrint      = printFnc }
-  r <- runReaderT (runExceptT m) config
-  case r of
-    Left (Failure n msg) -> do
-      forM_ msg (printFnc StderrDest)
-      return (Left n)
-    Right x -> return (Right x)
+runHapistrano sshOptions shell' printFnc m =
+  liftIO $ do
+    let config =
+          Config
+            { configSshOptions = sshOptions
+            , configShellOptions = shell'
+            , configPrint = printFnc
+            }
+    r <- runReaderT (runExceptT m) config
+    case r of
+      Left (Failure n msg) -> do
+        forM_ msg (printFnc StderrDest)
+        return (Left n)
+      Right x -> return (Right x)
 
 -- | Fail returning the following status code and message.
-
 failWith :: Int -> Maybe String -> Hapistrano a
 failWith n msg = throwError (Failure n msg)
 
@@ -71,15 +72,17 @@ failWith n msg = throwError (Failure n msg)
 -- __NOTE:__ the commands executed with 'exec' will create their own pipe and
 -- will stream output there and once the command finishes its execution it will
 -- parse the result.
-
-exec :: forall a. Command a => a -> Hapistrano (Result a)
+exec ::
+     forall a. Command a
+  => a
+  -> Hapistrano (Result a)
 exec typedCmd = do
   let cmd = renderCommand typedCmd
   (prog, args) <- getProgAndArgs cmd
-  parseResult (Proxy :: Proxy a) <$> exec' cmd (readProcessWithExitCode prog args "")
+  parseResult (Proxy :: Proxy a) <$>
+    exec' cmd (readProcessWithExitCode prog args "")
 
 -- | Same as 'exec' but it streams to stdout only for _GenericCommand_s
-
 execWithInheritStdout :: Command a => a -> Hapistrano ()
 execWithInheritStdout typedCmd = do
   let cmd = renderCommand typedCmd
@@ -89,27 +92,22 @@ execWithInheritStdout typedCmd = do
     -- | Prepares a process, reads @stdout@ and @stderr@ and returns exit code
     -- NOTE: @strdout@ and @stderr@ are empty string because we're writing
     -- the output to the parent.
-    readProcessWithExitCode'
-      :: ProcessConfig stdin stdoutIgnored stderrIgnored
+    readProcessWithExitCode' ::
+         ProcessConfig stdin stdoutIgnored stderrIgnored
       -> IO (ExitCode, String, String)
     readProcessWithExitCode' pc =
-      SPT.withProcessTerm pc' $ \p -> atomically $
-        (,,) <$> SPT.waitExitCodeSTM p
-             <*> return ""
-             <*> return ""
-        where
-          pc' = SPT.setStdout SPT.inherit
-              $ SPT.setStderr SPT.inherit pc
+      SPT.withProcessTerm pc' $ \p ->
+        atomically $ (,,) <$> SPT.waitExitCodeSTM p <*> return "" <*> return ""
+      where
+        pc' = SPT.setStdout SPT.inherit $ SPT.setStderr SPT.inherit pc
 
 -- | Get program and args to run a command locally or remotelly.
-
 getProgAndArgs :: String -> Hapistrano (String, [String])
 getProgAndArgs cmd = do
   Config {..} <- ask
   return $
     case configSshOptions of
-      Nothing ->
-        (renderShell configShellOptions, ["-c", cmd])
+      Nothing -> (renderShell configShellOptions, ["-c", cmd])
       Just SshOptions {..} ->
         ("ssh", sshArgs ++ [sshHost, "-p", show sshPort, cmd])
     where
@@ -117,29 +115,22 @@ getProgAndArgs cmd = do
       renderShell Zsh  = "zsh"
       renderShell Bash = "bash"
 
--- | Copy a file from local path to target server.
 
-scpFile
-  :: Path Abs File     -- ^ Location of the file to copy
-  -> Path Abs File     -- ^ Where to put the file on target machine
+-- | Copy a file from local path to target server.
+scpFile ::
+     Path Abs File -- ^ Location of the file to copy
+  -> Path Abs File -- ^ Where to put the file on target machine
   -> Hapistrano ()
-scpFile src dest =
-  scp' (fromAbsFile src) (fromAbsFile dest) ["-q"]
+scpFile src dest = scp' (fromAbsFile src) (fromAbsFile dest) ["-q"]
 
 -- | Copy a local directory recursively to target server.
-
-scpDir
-  :: Path Abs Dir      -- ^ Location of the directory to copy
-  -> Path Abs Dir      -- ^ Where to put the dir on target machine
+scpDir ::
+     Path Abs Dir -- ^ Location of the directory to copy
+  -> Path Abs Dir -- ^ Where to put the dir on target machine
   -> Hapistrano ()
-scpDir src dest =
-  scp' (fromAbsDir src) (fromAbsDir dest) ["-qr"]
+scpDir src dest = scp' (fromAbsDir src) (fromAbsDir dest) ["-qr"]
 
-scp'
-  :: FilePath
-  -> FilePath
-  -> [String]
-  -> Hapistrano ()
+scp' :: FilePath -> FilePath -> [String] -> Hapistrano ()
 scp' src dest extraArgs = do
   Config {..} <- ask
   let prog = "scp"
@@ -152,21 +143,19 @@ scp' src dest extraArgs = do
           Nothing -> ""
           Just x  -> x ++ ":"
       args = extraArgs ++ portArg ++ [src, hostPrefix ++ dest]
-  void (exec' (prog ++ " " ++ unwords args) (readProcessWithExitCode prog args ""))
+  void
+    (exec' (prog ++ " " ++ unwords args) (readProcessWithExitCode prog args ""))
 
 ----------------------------------------------------------------------------
 -- Helpers
-
 -- | A helper for 'exec' and similar functions.
-
-exec'
-  :: String            -- ^ How to show the command in print-outs
+exec' ::
+     String -- ^ How to show the command in print-outs
   -> IO (ExitCode, String, String) -- ^ Handler to get (ExitCode, Output, Error) it can change accordingly to @stdout@ and @stderr@ of child process
   -> Hapistrano String -- ^ Raw stdout output of that program
 exec' cmd readProcessOutput = do
   Config {..} <- ask
   time <- liftIO getZonedTime
-
   let timeStampFormat = "%T,  %F (%Z)"
       printableTime = formatTime defaultTimeLocale timeStampFormat time
       hostLabel =
@@ -178,18 +167,13 @@ exec' cmd readProcessOutput = do
       cmdInfo = colorizeString Green (cmd ++ "\n")
   liftIO $ configPrint StdoutDest (hostInfo ++ timestampInfo ++ cmdInfo)
   (exitCode', stdout', stderr') <- liftIO readProcessOutput
-  unless (null stdout') . liftIO $
-    configPrint StdoutDest stdout'
-  unless (null stderr') . liftIO $
-    configPrint StderrDest stderr'
+  unless (null stdout') . liftIO $ configPrint StdoutDest stdout'
+  unless (null stderr') . liftIO $ configPrint StderrDest stderr'
   case exitCode' of
-    ExitSuccess ->
-      return stdout'
-    ExitFailure n ->
-      failWith n Nothing
+    ExitSuccess   -> return stdout'
+    ExitFailure n -> failWith n Nothing
 
 -- | Put something “inside” a line, sort-of beautifully.
-
 putLine :: String -> String
 putLine str = "*** " ++ str ++ padding ++ "\n"
   where
