752e282b2248e7ee4c37e40c1b4bac95db130a0a
Sebastián Estrella <2049686+sestrella@users.noreply.github.com>
2021-07-16 11:31:18 -0500

    WIP - Adding hooks support

--- src/System/Hapistrano/EDSL.hs
+++ src/System/Hapistrano/EDSL.hs
@@ -15,19 +15,37 @@ data Config = Config
   , configBranch :: String
   }
 
+data Bar m = Bar
+  { barTasks :: M.HashMap String (m ())
+  , barBeforeHooks :: M.HashMap String [String]
+  , barAfterHooks :: M.HashMap String [String]
+  }
+
+emptyBar :: Bar m
+emptyBar = Bar mempty mempty mempty
+
+-- TODO: Rename type and constructors
+data Foo m
+  = FooTask (Task m)
+  | FooHook Hook
+
 data Task m
   = Namespace String [Task m]
   | Task String (m ())
 
-namespace :: String -> Writer [Task m] () -> Writer [Task m] ()
-namespace name tasks = tell [Namespace name $ execWriter tasks]
+data Hook
+  = After String String
+  | Before String String
+
+namespace :: String -> Writer [Task m] () -> Writer [Foo m] ()
+namespace name tasks = tell [FooTask $ Namespace name $ execWriter tasks]
 
 task :: String -> m () -> Writer [Task m] ()
 task name f = tell [Task name f]
 
-invoke :: MonadIO m => IORef (Context m) -> String -> m ()
+invoke :: MonadIO m => IORef (Bar m) -> String -> m ()
 invoke ref name = do
-  tasks <- liftIO $ readIORef ref
+  tasks <- barTasks <$> liftIO (readIORef ref)
   case M.lookup name tasks of
     Nothing -> undefined
     Just task -> task
@@ -35,17 +53,21 @@ invoke ref name = do
 
 run :: String -> IO ()
 run name = do
-  ref <- newIORef M.empty
-  let context = events $ execWriter $ tasks ref
+  ref <- newIORef emptyBar
+  let context = Bar
+        { barTasks = undefined
+        , barBeforeHooks = undefined
+        , barAfterHooks = undefined
+        }
   writeIORef ref context
-  case M.lookup name context of
+  case M.lookup name $ barTasks context of
     Nothing -> undefined
     Just task -> task
 
 type Context m = M.HashMap String (m ())
 
-tasks :: MonadIO m => IORef (Context m) -> Writer [Task m] ()
-tasks ref =
+defaultTasks :: MonadIO m => IORef (Bar m) -> Writer [Foo m] ()
+defaultTasks ref = do
   namespace "deploy" $ do
     task "starting" $ do
       liftIO $ putStrLn "Starting"
@@ -57,6 +79,11 @@ tasks ref =
 
     task "updating" $ do
       liftIO $ putStrLn "Updating"
+
+  {- task "deploy" $ do
+    liftIO $ putStrLn "Deploy"
+    invoke ref "deploy:starting" -}
+
 
 events :: [Task m] -> M.HashMap String (m ())
 events = M.fromList . concatMap (event Nothing)
