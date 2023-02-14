c0950f3c2322f3dd3d90def0af5fd32a2e02b562
Cristhian Motoche <CristhianMotoche@users.noreply.github.com>
2023-02-14 13:54:53 -0500

    fix: Filter for multiple targets (#213)
    * fix: Fix filter
    
    * ref: Use eitherDecode to get the parse error message

--- spec/System/HapistranoConfigSpec.hs
+++ spec/System/HapistranoConfigSpec.hs
@@ -1,19 +1,22 @@
-{-# LANGUAGE CPP             #-}
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE CPP               #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
 
 module System.HapistranoConfigSpec
   ( spec
   ) where
 
+import qualified Data.Aeson               as A
 import           System.Hapistrano.Config (Config (..), Target (..))
-import           System.Hapistrano.Types  (Shell (..),
-                                           Source (..), TargetSystem (..))
+import           System.Hapistrano.Types  (Shell (..), Source (..),
+                                           TargetSystem (..))
 
 import qualified Data.Yaml.Config         as Yaml
 #if MIN_VERSION_base(4,15,0)
 import           Path                     (mkAbsDir, mkRelDir, mkRelFile)
 #else
-import           Path                     (mkAbsDir, mkRelDir, mkRelFile, Abs, Rel, Dir, File)
+import           Path                     (Abs, Dir, File, Rel, mkAbsDir,
+                                           mkRelDir, mkRelFile)
 #endif
 import           Test.Hspec
 
@@ -35,6 +38,21 @@ spec =
       it "loads GitRepository as the configuration's source" $
         Yaml.loadYamlSettings ["fixtures/git_repository_config.yaml"] [] Yaml.useEnv
           >>= (`shouldBe` defaultConfiguration)
+
+    describe "Config From JSON instances" $ do
+      context "when contains unique targets" $
+        it "parses all unique targets" $
+          let eiHosts = fmap targetHost <$> configHosts <$> A.eitherDecode content
+              content =
+                "{\"targets\":[{\"host\":\"user@app1.com\"},{\"host\":\"user@app2.com\"}],\"deploy_path\":\"/tmp\",\"local_directory\":\"/\"}"
+          in eiHosts `shouldBe` Right ["user@app1.com", "user@app2.com"]
+
+      context "when contains duplicated targets" $
+        it "parses all only unique targets" $
+          let eiHosts = fmap targetHost <$> configHosts <$> A.eitherDecode content
+              content =
+                "{\"targets\":[{\"host\":\"user@app1.com\"},{\"host\":\"user@app1.com\"}],\"deploy_path\":\"/tmp\",\"local_directory\":\"/\"}"
+          in eiHosts `shouldBe` Right ["user@app1.com"]
 
 
 defaultConfiguration :: Config
