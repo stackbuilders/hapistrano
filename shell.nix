a93a511fcbd497e79aeabb6b78a9fe8125e0cf77
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-06-21 16:06:26 -0500

    Install GHC from pkgs.haskell

--- shell.nix
+++ shell.nix
@@ -1,13 +1,9 @@
 { pkgs, ghcVersion ? "9.0" }:
 
 let
-  haskellCompiler =
-    if pkgs.stdenv.isDarwin
-    then pkgs.haskell-nix.compiler
-    else pkgs.haskell.compiler;
   supportedGhcVersions = {
-    "8.10" = haskellCompiler.ghc8107;
-    "9.0" = haskellCompiler.ghc902;
+    "8.10" = pkgs.haskell.ghc8107;
+    "9.0" = pkgs.haskell.ghc902;
   };
 in
 pkgs.mkShell {
