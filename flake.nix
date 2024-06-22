2637ec86646ea4861bfb6a3a9418eb55aa913c57
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-06-22 03:04:18 -0500

    Allow flake config

--- flake.nix
+++ flake.nix
@@ -10,10 +10,6 @@
     extra-substituters = [ "https://cache.iog.io" ];
     extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
   };
-  # nixConfig = {
-  #   extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
-  #   extra-substituters = "https://devenv.cachix.org";
-  # };
 
   outputs = inputs@{ self, flake-utils, haskellNix, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system:
