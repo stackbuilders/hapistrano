8050d25d73b7e145d53752422be390d88a96976e
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2024-07-02 09:51:25 -0500

    Use different nixpkgs for each compiler-nix-name

--- flake.nix
+++ flake.nix
@@ -1,52 +1,59 @@
 {
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
-    haskellNix.url = "github:input-output-hk/haskell.nix";
-    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    haskellNix.url = "github:input-output-hk/haskell.nix/nix-tools-0.2.2";
+    nixpkgs-2205.follows = "haskellNix/nixpkgs-2205";
+    nixpkgs-unstable.follows = "haskellNix/nixpkgs-unstable";
   };
 
   nixConfig = {
-    allow-import-from-derivation = true;
+    allow-import-from-derivation = "true";
     extra-substituters = [ "https://cache.iog.io" ];
     extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
   };
 
-  outputs = inputs@{ self, flake-utils, haskellNix, nixpkgs }:
+  outputs = inputs@{ self, flake-utils, haskellNix, nixpkgs-2205, nixpkgs-unstable }:
     # https://input-output-hk.github.io/haskell.nix/tutorials/getting-started-flakes.html
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
+        mkCabalProject = final: compiler-nix-name: final.haskell-nix.cabalProject' {
+          inherit compiler-nix-name;
+          src = final.haskell-nix.haskellLib.cleanGit {
+            name = "hapistrano";
+            src = ./.;
+          };
+        };
+        pkgs-2205 = import nixpkgs-2205 {
           inherit system;
           inherit (haskellNix) config;
           overlays = [
             haskellNix.overlay
-            (final: prev:
-              let
-                mkCabalProject = compiler-nix-name: final.haskell-nix.cabalProject' {
-                  inherit compiler-nix-name;
-                  src = final.haskell-nix.haskellLib.cleanGit {
-                    name = "hapistrano";
-                    src = ./.;
-                  };
-                };
-              in
-              {
-                hapistrano-ghc8107 = mkCabalProject "ghc8107";
-                hapistrano-ghc902 = mkCabalProject "ghc902";
-              })
+            (final: prev: {
+              hapistrano = mkCabalProject final "ghc8107";
+            })
           ];
         };
-        flake-ghc8107 = pkgs.hapistrano-ghc8107.flake { };
-        flake-ghc902 = pkgs.hapistrano-ghc902.flake { };
+        pkgs-unstable = import nixpkgs-unstable {
+          inherit system;
+          inherit (haskellNix) config;
+          overlays = [
+            haskellNix.overlay
+            (final: prev: {
+              hapistrano = mkCabalProject final "ghc902";
+            })
+          ];
+        };
+        flake-ghc8107 = pkgs-2205.hapistrano.flake { };
+        flake-ghc902 = pkgs-unstable.hapistrano.flake { };
         overrideTestPackage = flake: flake.packages."hapistrano:test:test".overrideAttrs (_: {
           postFixup = ''
             wrapProgram $out/bin/test \
-              --set PATH ${pkgs.lib.makeBinPath [
-                pkgs.bash
-                pkgs.coreutils
-                pkgs.findutils
-                pkgs.git
-                pkgs.zsh
+              --set PATH ${pkgs-unstable.lib.makeBinPath [
+                pkgs-unstable.bash
+                pkgs-unstable.coreutils
+                pkgs-unstable.findutils
+                pkgs-unstable.git
+                pkgs-unstable.zsh
               ]}
           '';
         });
