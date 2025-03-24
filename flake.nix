cc1f30abbfc8c35931e60f68aa12dfa2805da6a4
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-03-24 16:21:31 -0500

    PoC: Replace haskell.nix with stacklock2nix
    Co-authored-by: Fabricio Mera <fmera@stackbuilders.com>
    Co-authored-by: David Becerra <dbecerra@stackbuilders.com>

--- flake.nix
+++ flake.nix
@@ -1,68 +1,29 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
-    haskellNix.url = "github:input-output-hk/haskell.nix";
-    nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs";
+    stacklock2nix.url = "github:cdepillabout/stacklock2nix";
   };
 
-  nixConfig = {
-    allow-import-from-derivation = "true";
-    extra-substituters = [
-      "https://cache.iog.io"
-      "https://cache.zw3rk.com"
-    ];
-    extra-trusted-public-keys = [
-      "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
-      "loony-tools:pr9m4BkM/5/eSTZlkQyRt57Jz7OMBxNSUiMC4FkcNfk="
-    ];
-  };
-
-  outputs = inputs@{ self, flake-utils, haskellNix, nixpkgs }:
-    # https://input-output-hk.github.io/haskell.nix/tutorials/getting-started-flakes.html
-    flake-utils.lib.eachDefaultSystem (system:
-      let
+  outputs =
+    {
+      nixpkgs,
+      stacklock2nix,
+    }:
+    let
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
         overlays = [
-          haskellNix.overlay
+          stacklock2nix.overlay
           (final: prev: {
-            hapistrano = final.haskell-nix.cabalProject' {
-              src = final.haskell-nix.haskellLib.cleanGit {
-                name = "hapistrano";
-                src = ./.;
-              };
-              # This is used by `nix develop .` to open a shell for use with
-              # `cabal`, `hlint` and `haskell-language-server`
-              shell.tools = {
-                cabal = {};
-                hlint = {};
-                haskell-language-server = {};
-              };
-              compiler-nix-name = "ghc966";
+            hapistrano-stacklock = final.stacklock2nix {
+              stackYaml = ./stack.yaml;
+              baseHaskellPkgSet = final.haskell.packages.ghc984;
             };
           })
-        ];       
-        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
-        flake = pkgs.hapistrano.flake { };
-      in rec {
-        apps = {
-          test = {
-            type = "app";
-            program = "${packages.test}/bin/test";
-          };
-        };
-        packages = {
-          default = flake.packages."hapistrano:exe:hap";
-          test = flake.packages."hapistrano:test:test".overrideAttrs (_: {
-            postFixup = ''
-              wrapProgram $out/bin/test \
-                --set PATH ${pkgs.lib.makeBinPath [
-                  pkgs.bash
-                  pkgs.coreutils
-                  pkgs.findutils
-                  pkgs.git
-                  pkgs.zsh
-                ]}
-            '';
-          });
-        };
-      });
+        ];
+      };
+    in
+    {
+      packages.x86_64-linux = pkgs;
+    };
 }
