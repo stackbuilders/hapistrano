ea1888f46e32642b28d0326d33ee41bf3347960e
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-03-25 01:54:18 -0500

    Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
   inputs = {
-    flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs";
     stacklock2nix.url = "github:cdepillabout/stacklock2nix";
   };
@@ -13,42 +12,54 @@
       stacklock2nix,
       ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          overlays = [
-            stacklock2nix.overlay
-            (final: prev: {
-              hapistrano-stacklock = final.stacklock2nix {
-                stackYaml = ./stack.yaml;
-                baseHaskellPkgSet = final.haskell.packages.ghc984;
-                all-cabal-hashes = final.fetchFromGitHub {
-                  owner = "commercialhaskell";
-                  repo = "all-cabal-hashes";
-                  rev = "299918adb3205b2dfe960bcdc79a9b1b300b11e6";
-                  sha256 = "sha256-Z0UJ78I3O8kWduNOqz7jASnR5XB8mwBDP0fVvjJoqOg=";
-                };
-                additionalHaskellPkgSetOverrides = hfinal: hprev: {
-                  hapistrano = final.haskell.lib.compose.overrideCabal (drv: {
-                    testToolDepends = drv.testToolDepends ++ [
-                      pkgs.git
-                      pkgs.zsh
-                    ];
-                  }) hprev.hapistrano;
-                };
+    let
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs
+          [
+            "aarch64-darwin"
+            "aarch64-linux"
+            "x86_64-darwin"
+            "x86_64-linux"
+          ]
+          (
+            system:
+            let
+              pkgs = import nixpkgs {
+                inherit system;
+                overlays = [
+                  stacklock2nix.overlay
+                  (final: prev: {
+                    hapistrano-stacklock = final.stacklock2nix {
+                      stackYaml = ./stack.yaml;
+                      baseHaskellPkgSet = final.haskell.packages.ghc984;
+                      all-cabal-hashes = final.fetchFromGitHub {
+                        owner = "commercialhaskell";
+                        repo = "all-cabal-hashes";
+                        rev = "299918adb3205b2dfe960bcdc79a9b1b300b11e6";
+                        sha256 = "sha256-Z0UJ78I3O8kWduNOqz7jASnR5XB8mwBDP0fVvjJoqOg=";
+                      };
+                      additionalHaskellPkgSetOverrides = hfinal: hprev: {
+                        hapistrano = final.haskell.lib.compose.overrideCabal (drv: {
+                          testToolDepends = drv.testToolDepends ++ [
+                            pkgs.git
+                            pkgs.zsh
+                          ];
+                        }) hprev.hapistrano;
+                      };
+                    };
+                    hapistrano = final.hapistrano-stacklock.pkgSet.hapistrano;
+                  })
+                ];
               };
-              hapistrano = final.hapistrano-stacklock.pkgSet.hapistrano;
-            })
-          ];
-        };
-      in
-      {
-        packages.default = pkgs.hapistrano;
-      }
-    )
-    // {
+            in
+            f pkgs
+          );
+    in
+    {
+      packages = forAllSystems (pkgs: {
+        default = pkgs.hapistrano;
+      });
       overlays.default = final: prev: {
         hapistrano = self.packages.${prev.system}.default;
       };
