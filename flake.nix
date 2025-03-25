949f07f780aca0eaa74b03bac7fae02ece65f691
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-03-25 00:27:29 -0500

    Disable tests for hapistrano package

--- flake.nix
+++ flake.nix
@@ -25,7 +25,17 @@
                 rev = "299918adb3205b2dfe960bcdc79a9b1b300b11e6";
                 sha256 = "sha256-Z0UJ78I3O8kWduNOqz7jASnR5XB8mwBDP0fVvjJoqOg=";
               };
+              additionalHaskellPkgSetOverrides = hfinal: hprev: {
+                hapistrano = final.haskell.lib.dontCheck hprev.hapistrano;
+              };
             };
+            # hapistrano = final.haskell.lib.compose.overrideCabal (drv: {
+            #   testToolDepends = [
+            #     pkgs.coreutils
+            #     pkgs.zsh
+            #   ];
+            # }) final.hapistrano-stacklock.pkgSet.hapistrano;
+            # hapistrano = final.haskell.lib.dontCheck final.hapistrano-stacklock.pkgSet.hapistrano;
             hapistrano = final.hapistrano-stacklock.pkgSet.hapistrano;
           })
         ];
