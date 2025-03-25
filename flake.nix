fe6482c7e04c9ca2180cd66026a446a5d888a908
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-03-24 19:02:51 -0500

    Add missing all-cabal-hashes

--- flake.nix
+++ flake.nix
@@ -12,13 +12,19 @@
     }:
     let
       pkgs = import nixpkgs {
-        system = "x86_64-linux";
+        system = "aarch64-darwin";
         overlays = [
           stacklock2nix.overlay
           (final: prev: {
             hapistrano-stacklock = final.stacklock2nix {
               stackYaml = ./stack.yaml;
               baseHaskellPkgSet = final.haskell.packages.ghc984;
+              all-cabal-hashes = final.fetchFromGitHub {
+                owner = "commercialhaskell";
+                repo = "all-cabal-hashes";
+                rev = "299918adb3205b2dfe960bcdc79a9b1b300b11e6";
+                sha256 = "sha256-Z0UJ78I3O8kWduNOqz7jASnR5XB8mwBDP0fVvjJoqOg=";
+              };
             };
             hapistrano = final.hapistrano-stacklock.pkgSet.hapistrano;
           })
@@ -26,6 +32,6 @@
       };
     in
     {
-      packages.x86_64-linux.default = pkgs.hapistrano;
+      packages.aarch64-darwin.default = pkgs.hapistrano;
     };
 }
