ac44f4d7ef61467c08cbe0307d0729f0701b8bdb
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-03-25 09:46:00 -0500

    Conditional sha256 based on platform

--- flake.nix
+++ flake.nix
@@ -33,7 +33,11 @@
                   owner = "commercialhaskell";
                   repo = "all-cabal-hashes";
                   rev = "299918adb3205b2dfe960bcdc79a9b1b300b11e6";
-                  sha256 = "sha256-Z0UJ78I3O8kWduNOqz7jASnR5XB8mwBDP0fVvjJoqOg=";
+                  sha256 =
+                    if final.stdenv.isLinux then
+                      "sha256-9nkHnZusYNDntpH9LrLTamY9BimRWfeX2m99lAuMCMI="
+                    else
+                      "sha256-Z0UJ78I3O8kWduNOqz7jASnR5XB8mwBDP0fVvjJoqOg=";
                 };
                 additionalHaskellPkgSetOverrides = hfinal: hprev: {
                   hapistrano = final.haskell.lib.compose.overrideCabal (drv: {
