9a9521ab54dc82021de9f33eaef2f0a03a2bbb50
Sebastian Estrella <2049686+sestrella@users.noreply.github.com>
2025-03-28 09:50:51 -0500

    Print warning for untested systems

--- flake.nix
+++ flake.nix
@@ -18,6 +18,10 @@
         "x86_64-darwin"
         "x86_64-linux"
       ];
+      unstestedSystems = [
+        "aarch64-linux"
+        "x86_64-darwin"
+      ];
       forAllSystems = f: nixpkgs.lib.genAttrs supportedSystems (system: f system);
       nixpkgsFor = forAllSystems (
         system:
@@ -31,9 +35,18 @@
       );
     in
     {
-      packages = forAllSystems (system: {
-        default = nixpkgsFor.${system}.hapistrano;
-      });
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default =
+            pkgs.lib.warnIf (builtins.any (x: x == system) unstestedSystems)
+              "'${system}' is not tested as part of the CI workflow; please report any issues you encounter while dealing with it."
+              pkgs.hapistrano;
+        }
+      );
       overlays.default = final: prev: {
         hapistrano = self.packages.${prev.system}.default;
       };
